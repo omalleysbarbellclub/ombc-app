0c02e0e03d2816f6c3c0c92ef23952ccc8c66cb4
omalleysbarbellclub <s.omalley88@gmail.com>
2025-06-12 18:33:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // tells Vite to start in root folder
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html') // CRUCIAL
-    }
-  }
-})
+  },
+});
